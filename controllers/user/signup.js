517a3eab59b06a92d44399b8d8b9f65bceacd59c
pmg7522 <pmg752290@gmail.com>
2022-07-07 09:46:58 +0900

    회원가입 작업

---
+++ controllers/user/signup.js
@@ -0,0 +1,41 @@
+const { user } = require("../../models")
+const crypto = require("crypto-js");
+
+/**
+ * @swagger
+ *  /user:
+ *    post:
+ *      tags:
+ *      - user
+ *      summary: 회원가입
+ *      description: 회원가입
+ *      consumes:
+ *      - application/json
+ *      produces:
+ *      - application/json
+ *      parameters:
+ *        - in: body
+ *          name: Body
+ *          required: true
+ *          description :
+ *          schema:
+ *              $ref: "#/definitions/userSignUp"
+ */
+module.exports = async (req, res, next) => {
+  const { username, name, password, authLevel } = req.body;
+
+  const hash = crypto.SHA256("password", process.env.SALT).toString();
+
+  const userData = await user.create({
+    username,
+    name,
+    password: hash,
+    authLevel,
+    status: 0,
+  });
+
+  return res.status(201).send({
+    message: "회원가입이 완료되었습니다.",
+    data: userData
+  });
+}
